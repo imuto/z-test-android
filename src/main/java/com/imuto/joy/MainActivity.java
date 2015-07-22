f356d69b9389fb4605d3b159a76d1756693641fa
imuto <imuto@qq.com>
2015-07-22 10:46:14 +0800

    Merge branch 'dev' of 121.199.41.149:youlin_android/sdk into dev
    Conflicts:
    	src/main/java/cn/youlin/sdk/config/Constants.java

--- src/main/java/com/imuto/joy/MainActivity.java
+++ src/main/java/com/imuto/joy/MainActivity.java
@@ -1,40 +1,112 @@
 package com.imuto.joy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.imuto.joy.DragHelper.DragHelperPage;
+import com.imuto.joy.base.AbsRecyclerAdapter;
+import com.imuto.joy.base.BaseActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BaseActivity {
 
 	private RecyclerView recyclerView;
+	private LinearLayoutManager layoutManager;
+
+	private Adapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		recyclerView = (RecyclerView) findViewById(R.id.mRecycler);
+
+		layoutManager = new LinearLayoutManager(this);
+
+		recyclerView.setLayoutManager(layoutManager);
+
+		recyclerView.setHasFixedSize(true);
+		adapter = new Adapter();
+
+		adapter.setOnItemClickListener(new AbsRecyclerAdapter.OnItemClickListener() {
+			@Override
+			public void onItemClick(View view, int position) {
+				Item item = adapter.getItem(position);
+
+				Intent intent = new Intent(MainActivity.this, item.toCls);
+
+				startActivity(intent);
+			}
+		});
+
+		recyclerView.setAdapter(adapter);
+
+		initData();
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.re, menu);
-		return true;
+	private void initData() {
+
+		ArrayList<Item> data = new ArrayList<>();
+		Item item = new Item("拖拽子View", DragHelperPage.class);
+		data.add(item);
+
+		adapter.setDataSet(data);
+		adapter.notifyDataSetChanged();
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+	private class Adapter extends AbsRecyclerAdapter<Item> {
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
+		public Adapter() {
+			super(MainActivity.this, new ArrayList<Item>(), R.layout.main_item);
 		}
 
-		return super.onOptionsItemSelected(item);
+		@Override
+		public AbsViewHolder onCreateViewHolder(ViewGroup parent, int viewType, View view) {
+			AbsViewHolder holder;
+			holder = new NormalHolder(view, this);
+			return holder;
+		}
+
+		@Override
+		public void onBindItemViewHolder(AbsViewHolder viewHolder, Item data, int position, int realPosition) {
+			OnBindNormalHolder((NormalHolder) viewHolder, data, position, realPosition);
+		}
+
+		private void OnBindNormalHolder(NormalHolder viewHolder, Item data, int position, int realPosition) {
+			viewHolder.title.setText(data.title);
+		}
+
+		private class NormalHolder extends AbsViewHolder {
+
+			TextView title;
+
+			public NormalHolder(View itemView, AbsRecyclerAdapter adapter) {
+				super(itemView, adapter);
+
+				title = (TextView) itemView.findViewById(R.id.title);
+			}
+		}
+
 	}
+
+	private class Item {
+
+		Item(String title, Class<? extends AppCompatActivity> toCls) {
+			this.title = title;
+			this.toCls = toCls;
+		}
+
+		private String title;
+		private Class<? extends AppCompatActivity> toCls;
+	}
+
 }
